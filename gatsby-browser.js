cfec685ebc32e48adad35668f3802990704162c0
cuparikun <cupparikun@gmail.com>
2022-01-25 17:00:56 +0100

    ...

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-// gatsby-browser.js || gatsby-ssr.js
-import React from 'react';
-import { MDXEmbedProvider } from 'mdx-embed';
-
-export const wrapRootElement = ({ element }) => {
-  return <MDXEmbedProvider>{element}</MDXEmbedProvider>;
-};
+//// gatsby-browser.js || gatsby-ssr.js
+//import React from 'react';
+//import { MDXEmbedProvider } from 'mdx-embed';
+//
+//export const wrapRootElement = ({ element }) => {
+//  return <MDXEmbedProvider>{element}</MDXEmbedProvider>;
+//};
