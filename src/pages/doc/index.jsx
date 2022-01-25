b5c680e58a286a67687f99a161928c610986dafe
cuparikun <cupparikun@gmail.com>
2022-01-25 08:24:45 +0100

    ..??..

--- src/pages/doc/index.jsx
+++ src/pages/doc/index.jsx
@@ -2,11 +2,43 @@ import * as React from "react";
 import { Link } from "gatsby";
 import Layout from '../../components/layout'
 import { graphql } from "gatsby";
+import { Button } from 'antd'
+import { GithubOutlined, TwitterOutlined } from '@ant-design/icons'
 
 const IndexPage = () => {
   return (
     <Layout pageHeading='Documentation' pageTitle='Doc'>
-      Mein Webdesign Workflow Presentation DOC
+      
+      <h2>A gatsby starter to create documentation websites</h2>
+      <p>
+        Mein Webdesign Workflow Presentation DOC/ This is a fork of{' '}
+        <a href="https://github.com/cvluca/gatsby-starter-markdown">
+          https://github.com/cvluca/gatsby-starter-markdown
+        </a>
+      </p>
+      <br />
+      <Button.Group size="large">
+        <Button
+          size="large"
+          icon={<TwitterOutlined />}
+          href="https://twitter.com/jannikbuschke"
+          target="_blank"
+        >
+          Twitter
+        </Button>
+        <Button
+          size="large"
+          icon={<GithubOutlined />}
+          href="https://github.com/jannikbuschke/gatsby-antd-docs"
+          target="_blank"
+        >
+          Github
+        </Button>
+
+        <Button type="primary" size="large">
+          <Link to="/docs/template/get-started/introduction"> Get Started</Link>
+        </Button>
+      </Button.Group>
     </Layout>
   )
 };
@@ -24,3 +56,4 @@ export const query = graphql`
     }
   }
 `
+
